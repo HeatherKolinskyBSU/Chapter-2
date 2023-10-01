39af10cf30077b994c5cd7fd1120e382a8b72879
HeatherKolinskyBSU <146090360+HeatherKolinskyBSU@users.noreply.github.com>
2023-10-01 22:51:33 +0100

    Ch2-Ex1
    Exercise 1 from Chapter 2 of the pre-lecture C++ activities.
    Fully working code with annotations.

--- Ch2-Ex1.cpp
+++ Ch2-Ex1.cpp
@@ -3,17 +3,17 @@
 #include <conio.h>
 using namespace std;
 
-int main() {
+int main() {                                                 // Denotes start of main function.
     string correctAnswer;
     string userInput;
     correctAnswer = "paris";
     cout << "What is the capital city of France? " << endl;
-    getline(cin, userInput);
-    if(userInput == correctAnswer){
+    getline(cin, userInput);                                 // Stops execution until input detected.
+    if(userInput == correctAnswer){                          // Checks if user was correct.
         cout << "Correct!" << endl;
     } else{
         cout << "Incorrect!" << endl;
     }
     
-    return 0;
-}
+    return 0;                                                // Return statement.
+}                                                            // Ends main function.
